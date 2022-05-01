f8cff2aaf454bff4d5c525559e979e892bd4600e
Margit Tennosaar <46651865+margittennosaar@users.noreply.github.com>
2022-05-01 18:47:04 +0300

    fixed layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Form from './components/Form';
+import View from './components/View';
+import Popup from './components/Popup';
+
+class App extends Component {
+  state = {
+    inputData: {
+      firstname: '',
+      lastname: '',
+      phone: '',
+      role: '',
+      message: '',
+    },
+    showPopup: false,
+  };
+
+  inputHandler = (e) => {
+    this.setState({
+      inputData: { ...this.state.inputData, [e.target.name]: e.target.value },
+    });
+  };
+
+  popUpHandler = (e) => {
+    e.preventDefault();
+    this.setState({ showPopup: !this.state.showPopup });
+  };
+
+  closeHandler = () => {
+    window.location.reload();
+  };
+
+  render() {
+    return (
+      <>
+        {' '}
+        <div className="form_area">
+          <Form change={this.inputHandler} submit={this.popUpHandler} />
+        </div>
+        <View {...this.state.inputData} />
+        {this.state.showPopup && (
+          <Popup close={this.closeHandler} {...this.state.inputData} />
+        )}
+      </>
+    );
+  }
 }
 
 export default App;
