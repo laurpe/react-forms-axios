81e466ac95f7ec6c63a4923a4eea4131ef9a2aa5
laurpe <laura.perttila@edu.bc.fi>
2022-05-03 14:45:26 +0300

    convert app to functional component, useState, useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 
 import Form from "./components/Form";
 import View from "./components/View";
@@ -7,133 +7,117 @@ import Notes from "./components/Notes";
 import EditPopup from "./components/EditPopup";
 import axios from "axios";
 
-class App extends Component {
-    state = {
-        inputData: {
+const App = () => {
+    const [inputData, setInputData] = useState({
+        firstname: "",
+        lastname: "",
+        phone: "",
+        role: "",
+        message: "",
+    });
+    const [showPopup, setShowPopup] = useState(false);
+    const [notes, setNotes] = useState([]);
+    const [currentNote, setCurrentNote] = useState({});
+    const [showEditPopup, setShowEditPopup] = useState(false);
+
+    const inputHandler = (e) => {
+        setInputData({
+            ...inputData,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    const handleFormSubmit = async () => {
+        const response = await axios.post(
+            "http://localhost:3010/notes",
+            inputData
+        );
+        setInputData({
             firstname: "",
             lastname: "",
             phone: "",
             role: "",
             message: "",
-        },
-        showPopup: false,
-        notes: [],
-        currentNote: {},
-        showEditPopup: false,
-    };
-
-    inputHandler = (e) => {
-        this.setState({
-            inputData: {
-                ...this.state.inputData,
-                [e.target.name]: e.target.value,
-            },
         });
+        setShowPopup(!showPopup);
+        setNotes([...notes, response.data]);
     };
 
-    handleFormSubmit = async () => {
-        const response = await axios.post(
-            "http://localhost:3010/notes",
-            this.state.inputData
-        );
-        this.setState({
-            inputData: {
-                firstname: "",
-                lastname: "",
-                phone: "",
-                role: "",
-                message: "",
-            },
-            showPopup: !this.state.showPopup,
-            notes: [...this.state.notes, response.data],
-        });
-    };
-
-    popUpHandler = (e) => {
+    const popUpHandler = (e) => {
         e.preventDefault();
-        this.setState({ showPopup: !this.state.showPopup });
+        setShowPopup(!showPopup);
     };
 
-    closeHandler = () => {
+    const closeHandler = () => {
         window.location.reload();
     };
 
-    getNotes = async () => {
+    const getNotes = async () => {
         const response = await axios.get("http://localhost:3010/notes");
-        this.setState({ notes: response.data });
+        setNotes(response.data);
     };
 
-    componentDidMount() {
-        this.getNotes();
-    }
+    useEffect(() => {
+        getNotes();
+    }, []);
 
-    handleDelete = async (id) => {
+    const handleDelete = async (id) => {
         await axios.delete(`http://localhost:3010/notes/${id}`);
-        const notes = this.state.notes.filter((note) => note.id !== id);
-        this.setState({ notes: notes });
+        const newNotes = notes.filter((note) => note.id !== id);
+        setNotes(newNotes);
     };
 
-    handleUpdate = async (event) => {
+    const handleUpdate = async (event) => {
         event.preventDefault();
         await axios.put(
-            `http://localhost:3010/notes/${this.state.currentNote.id}`,
-            this.state.currentNote
+            `http://localhost:3010/notes/${currentNote.id}`,
+            currentNote
         );
-        const index = this.state.notes.findIndex(
-            (note) => note.id === this.state.currentNote.id
-        );
-        const notesCopy = [...this.state.notes];
-        notesCopy[index] = this.state.currentNote;
-        this.setState({ showEditPopup: false, notes: notesCopy });
+        const index = notes.findIndex((note) => note.id === currentNote.id);
+        const notesCopy = [...notes];
+        notesCopy[index] = currentNote;
+        setShowEditPopup(false);
+        setNotes(notesCopy);
     };
 
-    inputUpdateHandler = (e) => {
-        this.setState({
-            currentNote: {
-                ...this.state.currentNote,
-                [e.target.name]: e.target.value,
-            },
-        });
+    const inputUpdateHandler = (e) => {
+        setCurrentNote({ ...currentNote, [e.target.name]: e.target.value });
     };
 
-    handleShowUpdateForm = (note) => {
-        this.setState({ showEditPopup: true, currentNote: note });
+    const handleShowUpdateForm = (note) => {
+        setShowEditPopup(true);
+        setCurrentNote(note);
     };
 
-    render() {
-        return (
-            <>
-                <div className="form_area">
-                    <Form
-                        change={this.inputHandler}
-                        submit={this.popUpHandler}
-                    />
-                    <View {...this.state.inputData} />
-                </div>
-                <div>
-                    <Notes
-                        notes={this.state.notes}
-                        handleDelete={this.handleDelete}
-                        handleShowUpdateForm={this.handleShowUpdateForm}
-                    />
-                </div>
-                {this.state.showPopup && (
-                    <Popup
-                        close={this.closeHandler}
-                        submit={this.handleFormSubmit}
-                        {...this.state.inputData}
-                    />
-                )}
-                {this.state.showEditPopup && (
-                    <EditPopup
-                        submit={this.handleUpdate}
-                        {...this.state.currentNote}
-                        change={this.inputUpdateHandler}
-                    />
-                )}
-            </>
-        );
-    }
-}
+    return (
+        <>
+            <div className="form_area">
+                <Form change={inputHandler} submit={popUpHandler} />
+                <View {...inputData} />
+            </div>
+            <div>
+                <Notes
+                    notes={notes}
+                    handleDelete={handleDelete}
+                    handleShowUpdateForm={handleShowUpdateForm}
+                />
+            </div>
+            {showPopup && (
+                <Popup
+                    close={closeHandler}
+                    submit={handleFormSubmit}
+                    {...inputData}
+                />
+            )}
+            {showEditPopup && (
+                <EditPopup
+                    submit={handleUpdate}
+                    {...currentNote}
+                    change={inputUpdateHandler}
+                />
+            )}
+        </>
+    );
+};
 
 export default App;
